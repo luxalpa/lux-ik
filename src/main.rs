ad3e2be76ee4eb9a6695ab46aa6241eb684a8993
Luxalpa <git@luxalpa.io>
2022-12-03 13:55:17 +0100

    submodules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+
 pub fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        .run();
 }
